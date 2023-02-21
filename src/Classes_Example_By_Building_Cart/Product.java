58e53e582845a0ff1fab1fd11c63d68481d31e6d
Ghayoor <ghayoorhussain701@gmail.com>
2023-02-21 22:16:48 +0500

    Cart System

---
+++ src/Classes_Example_By_Building_Cart/Product.java
@@ -0,0 +1,39 @@
+package Classes_Example_By_Building_Cart;
+
+
+import java.math.BigDecimal;
+
+public class Product {
+
+    private String name;
+    private BigDecimal price;
+
+    public Product() {
+    }
+
+    public Product(String name, BigDecimal price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product [name=" + name + ", price=" + price + "]";
+    }
+}
